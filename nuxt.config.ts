fc6e7157eaecb5dc581ff2ced25e6257120c6e83
code-sekhar <sekharchaudhary5@gmail.com>
2026-04-10 15:36:41 +0530

    Fix static generate for Cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  nitro: {
+    preset: 'cloudflare-pages'
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
