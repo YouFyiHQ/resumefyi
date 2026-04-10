e504e57ae87dfd87dedfcc8183ee7134b18ed81b
code-sekhar <sekharchaudhary5@gmail.com>
2026-04-11 02:07:00 +0530

    Clean Repo

--- app/pages/index_t.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-if (process.client) {
-  window.location.href = '/editor'
-}
-</script>
-
-<template></template>
--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  ssr: false,
-  nitro: {
-    preset: 'cloudflare-pages'
-  },
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
